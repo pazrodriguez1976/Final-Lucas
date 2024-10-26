f10f74feb66b3261ddd15d170f395aff68901f6c
Mariela Presa <matematica.4.2021@gmail.com>
2024-10-26 11:21:25 -0300

    arreglosclientes y pedidos

--- arreglos.js
+++ arreglos.js
@@ -3,3 +3,11 @@ export const inventario = [
     { id: 2, nombre: "PantalónC", tipo: "pantalón", talla: "L", precio: 30, cantidad: 5 },
     { id: 3, nombre: "CamisaL", tipo: "camisa", talla: "XL", precio: 40, cantidad: 2 }
   ];
+
+  export const clientes = [
+    { id: 1, nombre: "Carlos García", correo: "carlos@gmail.com" },
+    { id: 2, nombre: "Ana Pérez", correo: "ana@gmail.com" }
+  ];
+  
+  export const pedidos = []; // Se agregarán los pedidos aquí
+  
