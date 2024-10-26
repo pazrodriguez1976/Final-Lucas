b05fe4303ed91a09a0a6cbad4a3126792fa6ecf8
Paz Rodriguez <pazrodriguez1976@gmail.com>
2024-10-26 11:19:47 -0300

    Agregue arreglo inventario

---
+++ arreglos.js
@@ -0,0 +1,5 @@
+export const inventario = [
+    { id: 1, nombre: "RemeraV", tipo: "remera", talla: "M", precio: 20, cantidad: 10 },
+    { id: 2, nombre: "PantalónC", tipo: "pantalón", talla: "L", precio: 30, cantidad: 5 },
+    { id: 3, nombre: "CamisaL", tipo: "camisa", talla: "XL", precio: 40, cantidad: 2 }
+  ];
